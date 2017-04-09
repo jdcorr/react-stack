b30d384c1b3b7f25db7f4074953a0e2bb19b02ce
JD Corr <jd@jdcorr.com>
2017-04-09 02:25:22 -0700

    [Boiler] React and webpack hooked up - Section 4 complete

--- src/client/index.js
+++
@@ -1,5 +0,0 @@
-import 'babel-polyfill'
-
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
-
-document.querySelector(APP_CONTAINER_SELECTOR).innterHTML = '<h1>Yo Webpack</h1>'
---
+++ src/client/ui-App/index.jsx
@@ -0,0 +1,8 @@
+
+// @flow
+
+import React from 'react'
+
+const App = () => <h1>React Boiler</h1>
+
+export default App
