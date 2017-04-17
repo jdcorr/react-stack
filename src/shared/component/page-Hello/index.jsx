4e5330e0aebbfbd6b3c168f6610d7051b138b891
JD Corr <jd@jdcorr.com>
2017-04-16 22:21:48 -0700

    [Server/Shared] refactoring components over to shared

---
+++ src/shared/component/page-Hello/index.jsx
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react'
+
+import HelloButton from '../ui-HelloButton'
+import Message from '../ui-HelloMessage'
+
+const HelloPage = () =>
+  <div>
+    <Message />
+    <HelloButton />
+  </div>
+
+export default HelloPage
---
+++ src/shared/component/page-HelloAsync/index.jsx
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react'
+
+import HelloAsyncButton from '../ui-HelloAsyncButton'
+import HelloAsyncMessage from '../ui-HelloAsyncMessage'
+
+const HelloAsyncPage = () =>
+  <div>
+    <HelloAsyncMessage />
+    <HelloAsyncButton />
+  </div>
+
+export default HelloAsyncPage
